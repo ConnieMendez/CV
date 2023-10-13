ac028895d39b9292e5635c4c18086bc42a1d846d
ConnieMendez <147756701+ConnieMendez@users.noreply.github.com>
2023-10-13 16:53:29 -0300

    Add files via upload

---
+++ common_variables.R
@@ -0,0 +1,28 @@
+# Common non-csv data between CV and resume
+
+# Header with description of what is trying to be accomplished with this CV/Resume
+intro_text <- "Interes en el rubro economico, financiero y programacion. Cuento con conocimientos academicos respectivos, y experiencia en analisis economicos e informes coyunturales, aplicacion
+de metodos cuantitativos, programacion estadistica. <br>
+**Principales Actividades**-produccion y analisis de estadisticas economicas coyunturales, optimizacion de procesos estadisticos mediante software R, trabajo con bases de datos de registros
+administrativos y encuestas.
+
+**Me describo** como una persona organizada, responsable, adaptable y proactiva. Buenas habilidades analiticas, aprendizaje rapido. Capacidad para trabajar en equipo o de manera individual.
+Me gusta mantenerme en constante aprendizaje.
+"
+
+
+# Language skills for skill bars visualization
+skills <- tribble(
+  ~skill,               ~level,
+  "R",                  4,
+  "SQL",                2.5,
+  "STATA",              4,
+  "Excel",              5,
+  "Python",             2
+)
+
+skill2 <- tribble(
+  ~skill,               ~level,
+  "Español",                  5,
+  "Inglés",                2.5
+)
